b6015f7e2517dc995db2aa490f534731113e38fb
DreiRivera49 <ethanrivera0409@gmail.com>
2026-05-06 10:34:06 +1200

    Added multiplication feature

--- calculator.py
+++ calculator.py
@@ -4,3 +4,7 @@ def subtract(a, b):
  return a - b
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+
+def multiply(a, b):
+ return a * b
+print("Multiplication:", multiply(5, 3))

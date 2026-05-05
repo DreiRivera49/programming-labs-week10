df0f3ef38d0f27b589a57b43640a6928fc58616e
DreiRivera49 <ethanrivera0409@gmail.com>
2026-05-06 11:48:57 +1200

    Added new list and function for student age

--- student_manager.py
+++ student_manager.py
@@ -1,10 +1,22 @@
-students = []
+name_list = []
+age_list = []
+
 def add_student(name):
- students.append(name)
+ name_list.append(name)
+
+def add_age(age):
+ age_list.append(age)
+ 
 def display_students():
- for s in students:
- print(s)
-add_student(f"{students}) John")
-add_student(f"{students}) Emma")
+ for name_dis in name_list:
+  print(name_dis)
+
+ for age_dis in age_list:
+  print(age_dis)
+
+add_student("John")
+add_age(21)
+add_student("Emma")
+add_age(19)
 
 display_students()
